5c2323a3e20e608d0708ccf902ad961acdb0840e
abrahampg057 <abrahampg057@gmail.com>
2026-03-11 13:08:03 -0700

    Tabla mejorada de usuarios

--- parcial-1-poo/examen-practico/Index.php
+++ parcial-1-poo/examen-practico/Index.php
@@ -27,6 +27,50 @@ try {
 <html>
 <head>
     <title>Lista de Usuarios</title>
+
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: #f4f6f9;
+            margin:0;
+            padding:0;
+        }
+
+        h2{
+            text-align:center;
+            margin-top:40px;
+            color:#333;
+        }
+    table{
+            border-collapse: collapse;
+            margin:40px auto;
+            width:70%;
+            background:white;
+            box-shadow:0px 4px 10px rgba(0,0,0,0.1);
+        }
+
+    th{
+            background-color:#2c3e50;
+            color:white;
+            padding:12px;
+            text-transform:uppercase;
+            font-size:14px;
+        }
+        td{
+            padding:10px;
+            text-align:center;
+            border-bottom:1px solid #ddd;
+        }
+
+        tr:hover{
+            background-color:#f2f2f2;
+        }
+
+        tr:nth-child(even){
+            background-color:#fafafa;
+        }
+    </style>
+
 </head>
 <body>
 
@@ -41,7 +85,7 @@ try {
     </tr>
 
     <?php
-    foreach ($usuarios as $u) {
+    foreach ($objusuarios as $u) {
         echo "<tr>";
         echo "<td>" . $u->getNombre() . "</td>";
         echo "<td>" . $u->getCorreo() . "</td>";
