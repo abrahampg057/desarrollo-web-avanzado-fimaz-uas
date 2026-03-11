b558e364bf22ed29f9d6ea3ce5aadf3e8e98e42d
abrahampg057 <abrahampg057@gmail.com>
2026-03-11 11:43:11 -0700

    se agregaron los getters

--- parcial-1-poo/examen-practico/Usuario.php
+++ parcial-1-poo/examen-practico/Usuario.php
@@ -1,15 +1,22 @@
 <?php
 class Usuario{
-    private $nombre;
-    private $correo;
+    private $vnombre;
+    private $vcorreo;
 
-    public function __construct($nombre, $correo) {
-        $this->nombre = $nombre;
-        $this->correo = $correo;
+    public function __construct($vnombre, $vcorreo) {
+        $this->nombre = $vnombre;
+        $this->correo = $vcorreo;
 
-        if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($vcorreo, FILTER_VALIDATE_EMAIL)) {
             throw new Exception("El correo no tiene un formato valido");
         }
 
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+    public function getCorreo() {
+        return $this->correo;
     }
 }
